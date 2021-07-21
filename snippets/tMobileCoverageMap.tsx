80b8dc12c32d226948b017ace2e6252b94b90686
Ben Herila <ben@herila.net>
2021-07-21 00:09:48 -0700

    Fix whitespace on t-mobile coverage map

--- snippets/tMobileCoverageMap.tsx
+++ snippets/tMobileCoverageMap.tsx
@@ -1,23 +1,25 @@
-﻿import React from 'react';
+﻿import React from "react";
 
 export function TMobileCoverageMap({}): JSX.Element {
-    return <b>
-        <a
-            href="https://www.t-mobile.com/coverage/coverage-map"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            T-Mobile Coverage Map
-        </a>
-        .&nbsp;
-    </b>;
+  return (
+    <b>
+      <a
+        href="https://www.t-mobile.com/coverage/coverage-map"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        T-Mobile Coverage Map
+      </a>
+      .&nbsp;
+    </b>
+  );
 }
 
 export function CoverageMapTerm({}) {
-    return (
-        <React.Fragment>
-            Coverage maps are an indicator and not a guarantee of coverage or network
-            access.
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      Coverage maps are an indicator and not a guarantee of coverage or network
+      access.
+    </React.Fragment>
+  );
 }
