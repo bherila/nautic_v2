ec76688b6065768f20baa7b55fe08a7ea66d4e0b
Ben Herila <ben@herila.net>
2021-07-21 00:09:02 -0700

    Add layout to registration page

--- pages/registration.tsx
+++ pages/registration.tsx
@@ -4,15 +4,21 @@ import {
   nauticAlertBroadbandVideo,
 } from "../lib/PlanOptions";
 import RegistrationForm from "../lib/RegistrationForm";
+import Header from "../snippets/header";
+import Footer from "../snippets/footer";
 
 export default function RegistrationFormPage({}) {
   return (
-    <RegistrationForm
-      broadbandVideoAddOn={nauticAlertBroadbandVideo}
-      renderInstallDate={false}
-      renderDealerFields={false}
-      renderVesselType={false}
-      planOptions={nauticAlertPlanOptions}
-    />
+      <>
+        <Header />
+        <RegistrationForm
+            broadbandVideoAddOn={nauticAlertBroadbandVideo}
+            renderInstallDate={false}
+            renderDealerFields={false}
+            renderVesselType={false}
+            planOptions={nauticAlertPlanOptions}
+        />
+        <Footer />
+      </>
   );
 }
