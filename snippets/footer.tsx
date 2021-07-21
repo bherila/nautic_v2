e60e48006923999fb5029789a4dc1a11419a1c7d
Ben Herila <ben@herila.net>
2021-07-20 23:50:15 -0700

    Add footer

---
+++ snippets/footer.tsx
@@ -0,0 +1,134 @@
+﻿import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer-style-4">
+      <div className="top-footer w-hidden-tiny"></div>
+      <div className="container-8 w-container">
+        <div className="w-row">
+          <div className="w-col w-col-5">
+            <div className="div-block-30">
+              <h4 className="heading-white-copy margin-footer heading-gray">
+                About Us
+              </h4>
+              <div className="div-block-31">
+                <p className="darker">
+                  NearshoreNetworks brings innovation to vessel communications
+                  by merging on demand Satellite and 4G/LTE solutions with
+                  flexible terms of service and delivery.
+                </p>
+              </div>
+              <div className="html-embed w-embed">
+                <a
+                  href="https://www.facebook.com/nearshorenetworks/"
+                  target="_blank"
+                >
+                  <img
+                    src="https://niftybuttons.com/rkc-social-set-1/facebook.png"
+                    align="left"
+                    border="0"
+                    style={{ margin: "1px" }}
+                  />
+                </a>
+                <a href="https://twitter.com/BobM_Global" target="_blank">
+                  <img
+                    src="https://niftybuttons.com/rkc-social-set-1/twitter.png"
+                    border="0"
+                    style={{ margin: "1px" }}
+                    align="left"
+                  />
+                </a>
+                <a
+                  href="https://www.youtube.com/channel/UCx7phI3rdYJDbSI3WvZkA3w"
+                  target="_blank"
+                >
+                  <img
+                    src="https://niftybuttons.com/rkc-social-set-1/youtube.png"
+                    border="0"
+                    style={{ margin: "1px" }}
+                    align="left"
+                  />
+                </a>
+                <a
+                  href="https://www.linkedin.com/company/nearshorenetworks/"
+                  target="_blank"
+                >
+                  <img
+                    src="https://niftybuttons.com/rkc-social-set-1/linkedin.png"
+                    border="0"
+                    style={{ margin: "1px" }}
+                    align="left"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="w-col w-col-3"></div>
+          <div className="w-col w-col-4">
+            <div className="div-block-4">
+              <h4 className="heading-white margin-footer">Connect</h4>
+              <a
+                href="../../contact-us.html"
+                className="button-icon arrow w-button"
+              >
+                Book a Consultation
+              </a>
+              <div className="top-content tt">
+                <div className="troll-free-text inside-navigation white">
+                  <span className="number dark more">
+                    <a
+                      href="mailto:comms@nearshorenetworks.com?subject=Nearshorenetworks%20-%20Contact%20Request"
+                      className="footer-linkx"
+                    >
+                      comms@nearshorenetworks.com
+                    </a>
+                  </span>
+                </div>
+                <div className="top-content">
+                  <div className="troll-free-text inside-navigation white">
+                    <span className="number dark more">
+                      <a href="tel:+1844526-2627" className="footer-linkx">
+                        +1 844 526-2627
+                      </a>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="footer-divider"></div>
+        <div className="row-5 w-row">
+          <div className="column-40 w-col w-col-3">
+            <img
+              src="../../images/nearshorenetworks-logo-light.png"
+              width="145"
+              id="footer-logo"
+              alt=""
+              className="image-2"
+            />
+          </div>
+          <div className="column-39 w-col w-col-9">
+            <div className="w-row">
+              <div className="w-col w-col-6">
+                <div className="text-block-15">
+                  Nearshorenetworks, Inc. all rights reserved, 2021
+                </div>
+              </div>
+              <div className="column-68 w-col w-col-3">
+                <a href="../../privacy-policy.html" className="link-5">
+                  Privacy Policy
+                </a>
+              </div>
+              <div className="column-67 w-col w-col-3">
+                <a href="../../terms-conditions.html" className="link-5">
+                  Terms &amp; Conditions
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
