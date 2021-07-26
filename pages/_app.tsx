0b76ce85e57797d287517a1eaed2932531687ee2
Ben Herila <ben@herila.net>
2021-07-26 16:27:01 -0400

    Allow sync scripts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,15 +28,13 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="google-site-verification"
         />
         <script
-          async
           src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=5a29d8f7c76e0b0001d9eac5"
           type="text/javascript"
           integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-          crossOrigin={"anonymous"}
+          crossOrigin="anonymous"
         />
-        <script async type="text/javascript" src="/js/nearshorenetworks.js" />
+        <script type="text/javascript" src="/js/nearshorenetworks.js" />
         <script
-          async
           src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"
           type="text/javascript"
         />
