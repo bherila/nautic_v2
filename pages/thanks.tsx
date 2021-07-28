ff4d22e396ece482b644d6481f466694dca86112
Ben Herila <ben@herila.net>
2021-07-27 23:49:34 -0400

    Update thanks page

--- pages/thanks.tsx
+++ pages/thanks.tsx
@@ -5,8 +5,8 @@ export default function ThanksPage({}) {
     <>
       <h1 className="heading-39">Success!</h1>
       <div className="w-container">
-        <div style={{ padding: "70px" }}>
-          We will be in touch soon with next steps.
+        <div style={{ padding: "70px", textAlign: "center", fontSize: "20pt" }}>
+          Thank you! Your submission has been received!
         </div>
       </div>
     </>
