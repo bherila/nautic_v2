2de2f38468e59b1ff332e814dc063d83388289ec
Ben Herila <ben@herila.net>
2021-08-06 22:26:19 -0700

    Add layout to thanks page

--- src/pages/thanks.tsx
+++ src/pages/thanks.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import Layout from "./components/layout";
 
 export default function ThanksPage({}) {
   return (
-    <>
+    <Layout>
       <h1 className="heading-39">Success!</h1>
       <div className="w-container">
         <div style={{ padding: "70px", textAlign: "center", fontSize: "20pt" }}>
           Thank you! Your submission has been received!
         </div>
       </div>
-    </>
+    </Layout>
   );
 }
