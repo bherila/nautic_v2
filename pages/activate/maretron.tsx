947a35360b50a7bb74eb1b361f3608cc83e51007
Ben Herila <ben@herila.net>
2021-08-06 20:48:22 -0700

    Maretron page layout

--- pages/activate/maretron.tsx
+++ pages/activate/maretron.tsx
@@ -2,11 +2,29 @@
 import { maretronOptions } from "../../lib/PlanOptions";
 import RegistrationForm from "../../lib/RegistrationForm";
 import TermsContentMaretron from "../../snippets/TermsContentMaretron";
+import Image from "next/image";
 
 export default function RegistrationFormPage({}) {
   return (
     <main>
-      <h1 className="heading-39">Maretron SMS100 Activation Page</h1>
+      <div
+        className="w-container"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          paddingTop: "20px",
+        }}
+      >
+        <div style={{ display: "block" }}>
+          <h1>SMS100 Activation Page</h1>
+          <p>All fields are mandatory unless indicated</p>
+        </div>
+        <div style={{ display: "block" }}>
+          <Image src="/img/Maretron-Logo.png" width={300} height={69} />
+        </div>
+      </div>
       <div className="w-container">
         <div
           className="container-21 w-container"
