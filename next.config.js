00cc742940b3420ab93922d365fb49265fb6a4ff
Ben Herila <ben+gh@herila.net>
2026-05-13 10:02:07 -0700

    Redirect pages to Square checkout

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const checkoutUrl =
+  'https://checkout.square.site/merchant/MLF75XW78VVEB/checkout/PDRM3QG6ADMN46WJZ6G7GFT7'
+
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source:
+          '/((?!api|_next/static|_next/image|favicon.ico|favicon-16x16.png|favicon-32x32.png|apple-touch-icon.png|site.webmanifest|robots.txt).*)',
+        destination: checkoutUrl,
+        permanent: false,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
