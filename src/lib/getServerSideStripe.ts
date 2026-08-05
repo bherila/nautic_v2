6419e01299b47789729f8782ccea1ddc647a8496
Ben Herila <berty_coder@herila.net>
2026-08-05 01:52:05 +0000

    chore: update dependencies for security

--- src/lib/getServerSideStripe.ts
+++ src/lib/getServerSideStripe.ts
@@ -3,6 +3,6 @@ import Stripe from 'stripe'
 export default function getServerSideStripe() {
   return new Stripe(
     process.env.STRIPE_SECRET_KEY || 'sk_test_dXH7pAFCTeRjL39fWSwwYcQd',
-    { apiVersion: '2026-04-22.dahlia' },
+    { apiVersion: '2026-07-29.dahlia' },
   )
 }
